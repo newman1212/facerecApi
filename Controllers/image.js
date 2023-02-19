6105aae73b87496e928615ab937e0d3d8235321e
newman1212 <emmanuelseworh@gmail.com>
2023-02-19 00:07:36 +0000

    adding new files

---
+++ Controllers/image.js
@@ -0,0 +1,77 @@
+const Clarifai = require('clarifai');
+// console.log(Clarifai)
+
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 7bc118c5a5c245c0a6ecbb7269b957b5");
+
+// const app = new Clarifai.App({
+//  apiKey: '6e883bcb9dc9476da42964122e04ba19' 
+//  // '7bc118c5a5c245c0a6ecbb7269b957b5'
+// });
+
+// // 8bbf99ece230491ebb2ee4d52e3e5937
+
+
+
+
+
+
+
+
+apiCallHandler = (req,res) => {
+
+stub.PostModelOutputs(
+    {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id:
+        'face-detection',
+
+        inputs: [{data: {image: {url: req.body.input}}}]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        }
+
+        if (response.status.code !== 10000) {
+            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+            return;
+        }
+
+        console.log("Predicted concepts, with confidence values:",
+            response.outputs[0].data.regions[0].region_info.bounding_box)
+        // for (const c of ) {
+        //     console.log(c.name + ": " + c.value);
+        //     console.log(response);
+        // }
+        res.json(response)
+    }
+);
+
+
+
+ // .then(data=>res.json(data))
+ //  .catch(err => res.status(400).json('unable to work with API'));
+}
+
+
+const imageFormHandler =(req,res,db)=>{
+	const {id} = req.body;
+	db('users').where('id','=',id)
+	.increment('entries',1)
+	.returning('entries')
+	.then(entries=> {res.json(entries[0].entries)})
+		.catch(err=> res.status(400).json("unable to get entries"))
+}
+
+module.exports = {
+	imageFormHandler:imageFormHandler,
+	apiCallHandler:apiCallHandler
+
+}
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./Controllers/Register');
+const signin = require('./Controllers/signin');
+const image = require('./Controllers/image');
+const profile = require('./Controllers/profile');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : '0574200952',
+    database : 'facerecdb'
+  }
+});
+
+
+
+const app = express(); 
+// /--to create the app--/
+
+app.use(bodyParser.json()); //to make the req.body accessible or readable to the backend.
+
+app.use(cors()); //this is to bypass the browser's CORS(cross-origin sharing) policy...this syntax however means all
+// websites have access to the server.
+
+//AN IDEAL SYNTAX FOR USING cors WOULD BE :
+// app.use(cors({
+//     origin: ['website_1', 'website2']
+// })); 
+//(where website_1 & website_2 are the websites given access)..we could add more sites if we wish.
+
+// app.get('/',(req,res)=> { 
+// 	res.send(database.users)
+// })
+
+app.post('/signin',(req,res)=> {
+  signin.signInHandler(req,res,db,bcrypt)
+})
+
+app.post('/register', (req, res) => {
+register.registerHandler(req,res,db,bcrypt)
+}) //this is called dependency injection
+
+app.get('/profile/:id',(req,res) => {
+  profile.profileHandler(req,res,db)
+})
+
+app.put('/image',(req,res) => {image.imageFormHandler(req,res,db);
+})
+
+
+app.post('/imageUrl',(req,res) => {image.apiCallHandler(req,res);
+})
+
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+	console.log(`Server is listening on ${PORT}`)
+});
+
+// console.log(PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
