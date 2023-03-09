2329d5647fd9e3e727a42eb526d67d34bf9a801c
newman1212 <emmanuelseworh@gmail.com>
2023-03-09 18:42:46 +0000

    fix

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ const profile = require('./Controllers/profile');
 
 
 const db = knex({
-  client: 'pg',
+  // client: 'pg',
   connection: {
-    host : process.env.INTERNAL_DATABASE_URL,
+    host : 'postgres://user:ZUCzoA3fIJZJPCRD5Mi66KDlIlKjy64K@dpg-cg0iolo2qv2bfopr7m4g-a/facerecdb_7ffq', 
     port : 5432,
     // hostname: 'dpg-cg0iolo2qv2bfopr7m4g-a'
     username : 'user',
