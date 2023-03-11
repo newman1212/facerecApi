42622670a23a345dcd42947587ec2c69c36bff0c
newman1212 <emmanuelseworh@gmail.com>
2023-03-11 14:40:49 +0000

    fix

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const knex = require('knex');
@@ -25,7 +25,7 @@ const db = knex({
   connection: {
     host : 'postgres://user:ZUCzoA3fIJZJPCRD5Mi66KDlIlKjy64K@dpg-cg0iolo2qv2bfopr7m4g-a/facerecdb_7ffq', 
     port : 5432,
-    Hostname: 'dpg-cg0iolo2qv2bfopr7m4g-a',
+    hostname: 'dpg-cg0iolo2qv2bfopr7m4g-a',
     username : 'user',
     password : 'ZUCzoA3fIJZJPCRD5Mi66KDlIlKjy64K',
     database : 'facerecdb_7ffq'
