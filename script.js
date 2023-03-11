55f7db10df8c1d5aac7e262e635d9333a2bcd7cf
newman1212 <emmanuelseworh@gmail.com>
2023-03-11 17:16:34 +0000

    fix

--- script.js
+++ script.js
@@ -25,8 +25,7 @@ const db = knex({
   connection: {
     host : 'postgres://user:ZUCzoA3fIJZJPCRD5Mi66KDlIlKjy64K@dpg-cg0iolo2qv2bfopr7m4g-a/facerecdb_7ffq', 
     port : 5432,
-    hostname: 'dpg-cg0iolo2qv2bfopr7m4g-a',
-    username : 'user',
+    user : 'user',
     password : 'ZUCzoA3fIJZJPCRD5Mi66KDlIlKjy64K',
     database : 'facerecdb_7ffq'
   }
